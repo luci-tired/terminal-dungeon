84a71d4b6534862873e873b62a0ae4f0fea54a1d
luci-tired <skytalegamer@gmail.com>
2022-05-14 19:07:56 -0600

    Created the Nothing pathway

--- nothingPath.cs
+++ nothingPath.cs
@@ -4,6 +4,18 @@ namespace Nothing
 {
     class NothingClass
     {
-        
+        static public int nothingPath()
+        {
+            // Picks a random line from a array
+            string[] lines = {"You trip on a rock.", "You find and open a door, behind it is a wall.", "goop", "You hear distant voices.", "No bitches???", "WYDŮPIEJ", "-_-", "You step on an ant.", "You wonder if you will ever get out of this cave.", "Im am getting closer to your current location."};
+            Random rnd = new Random();
+            int index = rnd.Next(lines.Length); // Generates a random index less than the size of the array
+            Console.WriteLine(lines[index]);
+
+            // Gives 2-3 xp to the player
+            int xp = rnd.Next(2, 4);
+            Console.WriteLine(xp + "+ XP");
+            return xp;
+        }
     }
 }
