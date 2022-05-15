4f06ba684f4467412ffc191fdcc9f86c657fcce9
luci-tired <skytalegamer@gmail.com>
2022-05-15 13:11:30 -0600

    some styling changes

--- nothingPath.cs
+++ nothingPath.cs
@@ -7,14 +7,16 @@ namespace Nothing
         static public int nothingPath()
         {
             // Picks a random line from a array
-            string[] lines = {"You trip on a rock.", "You find and open a door, behind it is a wall.", "goop", "You hear distant voices.", "No bitches???", "WYDŮPIEJ", "-_-", "You step on an ant.", "You wonder if you will ever get out of this cave.", "Im am getting closer to your current location."};
+            string[] lines = {"You trip on a rock.", "You find and open a door, behind it is a wall.", "goop", "You hear distant voices.", "No bitches???", "WYDUPIEJ", "-_-", "You step on an ant.", "You wonder if you will ever get out of this cave.", "Im am getting closer to your current location."};
             Random rnd = new Random();
             int index = rnd.Next(lines.Length); // Generates a random index less than the size of the array
             Console.WriteLine(lines[index]);
 
             // Gives 2-3 xp to the player
+            Console.ForegroundColor = ConsoleColor.Green;
             int xp = rnd.Next(2, 4);
             Console.WriteLine(xp + "+ XP");
+            Console.ResetColor();
             return xp;
         }
     }
