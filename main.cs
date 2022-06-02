6a05b85d98ab4529375073eda907f0e6e19b4249
luci <skytalegamer@gmail.com>
2022-06-02 11:29:45 -0600

    changed selectnumb to not show user input

--- main.cs
+++ main.cs
@@ -234,51 +234,61 @@ namespace TDMain
 
         public static string SelectNumb()
         {
-            switch (Console.ReadLine())
+            ConsoleKey _key = Console.ReadKey(true).Key;
+
+            if (_key == ConsoleKey.D1)
             {
-                case "1":
-                    return "1";
-                break;
+                return "1";
+            }
 
-                case "2":
-                    return "2";
-                break;
+            else if (_key == ConsoleKey.D2)
+            {
+                return "2";
+            }
 
-                case "3":
-                    return "3";
-                break;
+            else if (_key == ConsoleKey.D3)
+            {
+                return "3";
+            }
 
-                case "4":
-                    return "4";
-                break;
+            else if (_key == ConsoleKey.D4)
+            {
+                return "4";
+            }
 
-                case "5":
-                    return "5";
-                break;
+            else if (_key == ConsoleKey.D5)
+            {
+                return "5";
+            }
 
-                case "6":
-                    return "6";
-                break;
+            else if (_key == ConsoleKey.D6)
+            {
+                return "6";
+            }
 
-                case "7":
-                    return "7";
-                break;
+            else if (_key == ConsoleKey.D7)
+            {
+                return "7";
+            }
 
-                case "8":
-                    return "8";
-                break;
+            else if (_key == ConsoleKey.D8)
+            {
+                return "8";
+            }
 
-                case "9":
-                    return "9";
-                break;
+            else if (_key == ConsoleKey.D9)
+            {
+                return "9";
+            }
 
-                case "0":
-                    return "0";
-                break;
+            else if (_key == ConsoleKey.D0)
+            {
+                return "0";
+            }
 
-                default:
-                    return SelectNumb();
-                break;
+            else
+            {
+                return SelectNumb();
             }
         }
     }
