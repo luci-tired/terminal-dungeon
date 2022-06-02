0744388d69705fefbb33faeea1fe25cf32f80a6a
luci <skytalegamer@gmail.com>
2022-06-02 11:28:46 -0600

    changed placeholder

--- pixelArt.cs
+++ pixelArt.cs
@@ -12,6 +12,51 @@ namespace Art
                 Console.Write("▄");
             }
             Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("▄");
+            }
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("▄");
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("▄");
+            }
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("▄");
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("▄");
+            }
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("▄");
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("▄");
+            }
+            Console.WriteLine();
             Console.ResetColor();
         }
     }
